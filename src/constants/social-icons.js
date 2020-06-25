5cc17adc810f7528d0d98969fa03d055b5c6d1a9
Kate Mills <kate.mills1124@gmail.com>
2020-06-25 14:23:19 -0700

    Constants(links, social-icons)

---
+++ src/constants/social-icons.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FaFacebookF, FaTwitterSquare, FaInstagram } from 'react-icons/fa'
+
+export default [
+  {
+    icon: <FaFacebookF />,
+    url: 'https://facebook.com',
+  },
+  {
+    icon: <FaTwitterSquare />,
+    url: 'https://twitter.com',
+  },
+  {
+    icon: <FaInstagram />,
+    url: 'https://instagram.com',
+  },
+]
