e124b4efaf695c2b11765343e202c5ac0a7537c6
Kate Mills <kate.mills1124@gmail.com>
2020-06-25 11:21:26 -0700

    Added prettier

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import classes from './logo.module.css'
 
-const Logo = ({src, alt}) => {
-  return(
+const Logo = ({ src, alt }) => {
+  return (
     <div className={classes.Logo}>
-      <a href="./"><img src={src} alt={alt}/></a>
+      <a href="./">
+        <img src={src} alt={alt} />
+      </a>
     </div>
-    
   )
 }
 export default Logo
