3ce826665d2f4f9aa2e1b617073031be1307d622
Kate Mills <kate.mills1124@gmail.com>
2020-06-25 15:49:26 -0700

    Added a skeleton page

---
+++ src/pages/SkeletonPage/Skeleton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+//import classes from './Skeleton.module.css'
+
+import Layout from '../../hoc/Layout'
+
+const Page = props => {
+  return(
+    <Layout>
+      <div>Hello from Skeleton</div>
+    </Layout>
+  )
+}
+
+export default Page
