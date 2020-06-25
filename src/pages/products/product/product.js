e3f18a097f3daa383d66460318b04c151ade8a33
Kate Mills <kate.mills1124@gmail.com>
2020-06-25 11:36:59 -0700

    Basic Products page

---
+++ src/pages/products/product/product.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import classes from './product.module.css'
+
+class Product extends Component {
+
+  render() {
+    return(
+      <div>
+        Single Product
+      </div>
+    )
+  }
+}
+
+export default Product
---
+++ src/pages/products/products.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './products.module.css'
+
+const Products = (props) => {
+  return (
+    <div>Products</div>
+  )
+}
+
+export default Products
