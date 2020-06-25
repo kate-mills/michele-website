f699ce4d174a20a37d929d9184764e7506cc5843
Kate Mills <kate.mills1124@gmail.com>
2020-06-25 11:40:49 -0700

    Bring pages into App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 import Logo from './components/logo/logo'
+
 import HomePage from './pages/homepage'
+import Products from './pages/products'
+import Product from './pages/products/product'
+import Error from './pages/error'
 
 import './App.css'
 
@@ -13,6 +17,9 @@ function App() {
         alt="Michele Corley Clinical Skincare Logo"
       />
       <HomePage />
+      <Products/>
+      <Product/>
+      <Error/>
     </>
   )
 }
