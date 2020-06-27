5d14d2fb4b1c3bdb6c016748020fbbe77d565681
Kate Mills <kate.mills1124@gmail.com>
2020-06-27 07:18:12 -0700

    Added state to Navbar and MENU toggle is a button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,11 +6,25 @@ import links from '../../constants/links'
 import {Link} from 'react-router-dom'
 
 class Navbar extends Component {
+  state = {
+    isOpen: false
+  }
+  handleToggle = () => {
+    this.setState({isOpen: !this.state.isOpen}, () => console.log(this.state.isOpen))
+  }
   render() {
     return (
       <nav>
         <div className={classes.gridUpperContainer}>
-          <div className={`${classes.gridUpperToggle} ${classes.gridItem}` }>MENU</div>
+          <div className={`${classes.gridUpperToggle} ${classes.gridItem}` }>
+            <button
+              type="button"
+              className={classes.navBtn}
+              onClick={this.handleToggle}
+            >
+            MENU
+            </button>
+          </div>
           <div className={classes.gridUpperLogo}><Logo/></div>
         </div>
         <ul className={classes.gridLowerContainer}>
