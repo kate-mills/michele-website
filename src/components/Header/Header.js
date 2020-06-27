31fe6746da78a3724907fe0217797cfd0a4bc62a
Kate Mills <kate.mills1124@gmail.com>
2020-06-27 15:37:43 -0700

    Removed Hero from Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,6 @@ import React from 'react'
 import classes from './Header.module.css'
 
 import Navbar from './Navbar'
-import Hero from './Hero'
 
 const Header = props => {
   return (
@@ -11,7 +10,6 @@ const Header = props => {
       <div className={classes.titleContainer}>
         <h1 className={classes.title}>{props.title}</h1>
       </div>
-      <Hero />
     </header>
   )
 }
