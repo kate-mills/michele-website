ddfd41d963a45705f3ba160c7fe704c40070beb1
Kate Mills <kate.mills1124@gmail.com>
2020-06-27 08:54:57 -0700

    Moved Layout css to App.css

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react'
-import classes from './Layout.module.css'
 
+import Aux from '../Aux'
 import Navbar from '../../components/Navbar'
 
 class Layout extends Component {
   render() {
     return (
-      <>
-        <main className={classes.mainContent}>
-          <Navbar />
-          <div className={classes.pageContent}>{this.props.children}</div>
-        </main>
-      </>
+      <Aux>
+        <Navbar />
+        <main>{this.props.children}</main>
+      </Aux>
     )
   }
 }
