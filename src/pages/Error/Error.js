ad39780bffedf69c55b4c096e4fa876f530d9ef5
Kate Mills <kate.mills1124@gmail.com>
2020-06-28 16:03:38 -0700

    Error page

--- src/pages/Error/Error.js
+++ src/pages/Error/Error.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import {Link} from 'react-router-dom'
+import classes from './Error.module.css'
 
-//import classes from './Error.module.css'
+const Error = () => {
+  return (
+    <section className={`${classes.errorPage} section`}>
+      <div className={`${classes.errorContainer} section`}>
+        <h1>oops! its a dead end</h1>
+        <Link to="/" className="btn btn-primary">
+          back home
+        </Link>
+      </div>
+    </section>
 
-const Error = props => {
-  return <div>Error Page</div>
+  )
 }
-
 export default Error
