d11de892b18a6a6365d512478947dae9ee049184
Kate Mills <kate.mills1124@gmail.com>
2020-06-28 14:24:41 -0700

    basic image

---
+++ src/components/Image/Image.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import classes from './Image.module.css'
+
+import defaultImage from '../../assets/images/3-women.jpg'
+
+const Image = props => {
+  return (
+    <article>
+      {
+       props.showDefault ? 
+         <div className={classes.default}></div>: 
+         <div/>
+      }
+      <img
+        className={classes.largeImage}
+        src={props.src}
+        alt={props.alt} />
+    </article>
+  )
+}
+
+Image.defaultProps = {
+  src: defaultImage,
+  alt: "Portrait of 3 women representing Michele Corley Clinical Skincare"
+}
+export default Image
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,11 +3,13 @@ import React from 'react'
 import classes from './HomePage.module.css'
 
 import Heading from '../../components/Heading'
+import Image from '../../components/Image'
 
 const HomePage = props => {
   return (
     <section>
       <Heading />
+      <Image showDefault/>
       <article className={classes.pageArticle}>images</article>
 
       <article>yes no</article>
